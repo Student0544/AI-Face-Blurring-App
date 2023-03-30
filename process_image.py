275957e02be4821e578abe1b8f23cc49b10bb34c
Student0544 <78926696+Student0544@users.noreply.github.com>
2023-03-29 23:36:01 -0400

    Update process_image.py

--- process_image.py
+++ process_image.py
@@ -66,15 +66,15 @@ def process_image(image_path: str, new_height: int, new_width: int, brighten: fl
           f"{errors} image{plural_noun} {plural_verb} unsuccessfully processed.")
 
 
-input_folder1 = r"C:\Users\cotyl\OneDrive\Desktop\CCIHP_icip\val_img"
-input_folder2 = r"C:\Users\cotyl\OneDrive\Desktop\CCIHP_icip\val_seg"
+input_folder1 = r"path_to_training_dataset_here"
+input_folder2 = r"path_to_segmented_dataset_here"
 new_height = 300
 new_width = 300
 brighten_factor = 15
 contrast_factor = 1
 
 # To process the images normally, run the first line below for the normal camera images, and run the second line for
-# the segmented images
+# the segmented images (this applies for the images from the CCIHP training and segmented dataset)
 # process_image(input_folder1, new_height, new_width)
 # process_image(input_folder2, new_height, new_width, brighten=brighten_factor)
 
